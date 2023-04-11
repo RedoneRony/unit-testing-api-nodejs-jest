07694699ea1a5acc27ff0c71c2e67af1d8808d05
Md.Redone Hassan <39440576+RedoneRony@users.noreply.github.com>
2023-04-11 14:13:53 +0600

    Initial commit

---
+++ routes/posts.js
@@ -0,0 +1,42 @@
+var express = require("express");
+const Post = require("../models/Post");
+const router = express.Router();
+
+const fs = require("fs");
+const util = require("util");
+const unlinkFile = util.promisify(fs.unlink);
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
+const { uploadFile, getFileStream } = require("../s3");
+
+router.get("/files/:key", (req, res) => {
+  const key = req.params.key;
+  const readStream = getFileStream(key);
+  readStream.pipe(res);
+});
+
+router.post("/files", upload.single("image"), async (req, res) => {
+  const file = req.file;
+
+  // uploading image
+  if (file !== undefined) {
+    const result = await uploadFile(file);
+    await unlinkFile(file.path);
+    res.send({ imagePath: `/images/${result.Key}` });
+  } else {
+    res.send("File Not Found");
+  }
+});
+
+router.delete("/files/:key", async (req, res) => {
+  const filename = req.params.key;
+  const aws = require("aws-sdk");
+  const s3 = new aws.S3();
+  await s3
+    .deleteObject({ Bucket: process.env.AWS_BUCKET_NAME, Key: filename })
+    .promise();
+  res.send("File Deleted Successfully");
+});
+
+module.exports = router;
